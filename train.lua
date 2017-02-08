49ae0399140be6a8c4803ca4d5e6af3270b50425
Richard Assar <richard.assar@gmail.com>
2017-02-08 14:19:35 +0000

    Port from nngraph to barebones nn model, network needs to be segmented for sampling

--- train.lua
+++ train.lua
@@ -1,13 +1,14 @@
 require 'nn'
 require 'rnn'
-require 'nngraph'
 require 'gnuplot'
 require 'image'
 require 'optim'
 require 'cunn'
 require 'cudnn'
 require 'audio'
+cudnn.fastest = true
 
+local learning_rate = 0.001
 local max_iter = 100
 local max_grad_norm = 0.1
 
@@ -19,7 +20,7 @@ local dim = big_dim
 local q_levels = 256
 local emb_size = 256
 
-local dat = audio.load('piano_note.wav'):view(-1)[{{1,1024}}]--torch.sin(i*4) + torch.sin(i*32)*0.2
+local dat = audio.load('piano_note.wav'):view(-1)[{{1,1024}}]
 dat:csub(dat:min())
 dat:div(dat:max())
 
@@ -34,65 +35,91 @@ local big_frames = big_input_sequences:unfold(1,big_frame_size,big_frame_size)
 local frames = input_sequences:unfold(1,frame_size,frame_size)
 prev_samples = prev_samples:unfold(1,frame_size,1)
 
-function big_frame_level_rnn(inp)
-    local L1 = inp 
-             - nn.View(1,-1):setNumInputDims(1)
-             - cudnn.GRU(big_frame_size, big_dim, 1)
-             - nn.View(-1):setNumInputDims(1)
+local big_frame_level_rnn = nn.Sequential()
+    :add(nn.View(1,-1):setNumInputDims(1))
+    :add(cudnn.GRU(big_frame_size, big_dim, 1))
+    :add(nn.View(-1):setNumInputDims(1))
+    :add(nn.ConcatTable()
+        :add(nn.Sequential()
+            :add(nn.Linear(big_dim, dim * big_frame_size / frame_size))
+            :add(nn.View(-1,dim))
+        )
+        :add(nn.Sequential()
+            :add(nn.Linear(big_dim, q_levels * big_frame_size))
+            :add(nn.View(-1,q_levels))
+        )
+    )
 
-    local output = L1
-                 - nn.Linear(big_dim, dim * big_frame_size / frame_size)
-                 - nn.View(-1,dim)
+local frame_level_rnn = nn.Sequential()
+    :add(nn.ParallelTable()
+        :add(nn.Linear(frame_size, dim))
+        :add(nn.Identity())
+    )
+    :add(nn.CAddTable())
+    :add(nn.View(1,-1):setNumInputDims(1))
+    :add(cudnn.GRU(dim, dim, 1))
+    :add(nn.View(-1):setNumInputDims(1))
+    :add(nn.Linear(dim, dim * frame_size))
+    :add(nn.View(-1,dim))
 
-    local independent_preds = L1
-                            - nn.Linear(big_dim, q_levels * big_frame_size)
-                            - nn.View(-1,q_levels)
 
-    return output, independent_preds
-end
+local sample_level_predictor = nn.Sequential()
+    :add(nn.ParallelTable()
+        :add(nn.Identity())
+        :add(nn.Sequential()
+            :add(nn.Contiguous())
+            :add(nn.View(-1,1))
+            :add(nn.LookupTable(q_levels, emb_size))
+            :add(nn.View(-1,frame_size*emb_size))
+            :add(nn.Linear(frame_size*emb_size, dim, false))
+        )
+    )
+    :add(nn.CAddTable())
+    :add(nn.Linear(dim,dim))
+    :add(cudnn.Tanh())
+    :add(nn.Linear(dim,dim))
+    :add(cudnn.Tanh())
+    :add(nn.Linear(dim,q_levels))
+    :add(cudnn.SoftMax())
 
-function frame_level_rnn(inp, other_inp)
-    local L2 = inp 
-             - nn.Linear(frame_size, dim)    
-            
-    local output = nn.CAddTable()({L2, other_inp})
-                 - nn.View(1,-1):setNumInputDims(1)
-                 - cudnn.GRU(dim, dim, 1)
-                 - nn.View(-1):setNumInputDims(1)
-                 - nn.Linear(dim, dim * frame_size)
-                 - nn.View(-1,dim)
+local net = nn.Sequential()
+    :add(nn.ParallelTable()
+        :add(big_frame_level_rnn)
+        :add(nn.Identity())
+        :add(nn.Identity())
+    )
+    :add(nn.ConcatTable()
+        :add(nn.Sequential()
+            :add(nn.ConcatTable()                
+                :add(nn.SelectTable(2))
+                :add(nn.Sequential()
+                    :add(nn.SelectTable(1))
+                    :add(nn.SelectTable(1))
+                )
+            )
+            :add(frame_level_rnn)
+        )
+        :add(nn.Identity())
+    )
+    :add(nn.ConcatTable()
+        :add(nn.Sequential()
+            :add(nn.ConcatTable()
+                :add(nn.SelectTable(1))
+                :add(nn.Sequential()
+                    :add(nn.SelectTable(2))
+                    :add(nn.SelectTable(3))
+                )
+            )
+            :add(sample_level_predictor)
+        )
+        :add(nn.Sequential()
+            :add(nn.SelectTable(2))
+            :add(nn.SelectTable(1))
+            :add(nn.SelectTable(2))
+        )
+    )
+    :cuda()
 
-    return output
-end
-
-function sample_level_predictor(frame_level_outputs, prev_samples)
-    local L3 = prev_samples
-             - nn.Contiguous() 
-             - nn.View(-1,1)
-             - nn.LookupTable(q_levels, emb_size)           
-             - nn.View(-1,frame_size*emb_size)
-             - nn.Linear(frame_size*emb_size, dim, false)
-
-    local output = nn.CAddTable()({L3, frame_level_outputs})
-                 - nn.Linear(dim,dim)
-                 - cudnn.Tanh()
-                 - nn.Linear(dim,dim)
-                 - cudnn.Tanh()
-                 - nn.Linear(dim,q_levels)
-                 - cudnn.SoftMax()
-
-    return output
-end
-
-local i1 = nn.Identity()()
-local i2 = nn.Identity()()
-local i3 = nn.Identity()()
-
-local big_frame_outputs,independent_preds = big_frame_level_rnn(i1)
-local frame_level_outputs = frame_level_rnn(i2, big_frame_outputs)
-local sample_level_outputs = sample_level_predictor(frame_level_outputs, i3)
-
-local net = nn.gModule({i1,i2,i3},{sample_level_outputs,independent_preds}):cuda()
 local param,dparam = net:getParameters()
 
 local linearLayers = net:findModules('nn.Linear')
@@ -107,59 +134,63 @@ function resetStates()
     end
 end
 
-local criterion = nn.ParallelCriterion()
-                    :add(nn.CrossEntropyCriterion())
-                    :add(nn.CrossEntropyCriterion())
-                    :cuda()
+function train()
+    local criterion = nn.ParallelCriterion()
+                        :add(nn.CrossEntropyCriterion())
+                        :add(nn.CrossEntropyCriterion())
+                        :cuda()
 
-local inp = {big_frames:cuda(),frames:cuda(),prev_samples:cuda()}
-local out = {target_sequences:cuda(),target_sequences:cuda()}
+    local inp = {big_frames:cuda(),frames:cuda(),prev_samples:cuda()}
+    local out = {target_sequences:cuda(),target_sequences:cuda()}
 
-local optim_config = {
-    learningRate = 0.001,
-}
+    local optim_config = {
+        learningRate = learning_rate,
+    }
 
-local loss = torch.DoubleTensor(max_iter)
+    local losses = torch.DoubleTensor(max_iter)
+    for i = 1,max_iter do
+        function feval(x)
+            if x ~= param then
+                param:copy(x)
+            end
 
-for i = 1,max_iter do
-    function feval(x)
-        if x ~= param then
-            param:copy(x)
+            net:zeroGradParameters()
+
+            resetStates()
+
+            local pred = net:forward(inp)
+            local loss = criterion:forward(pred,out)
+            net:backward(inp,criterion:backward(pred,out))
+
+            local grad_norm = dparam:norm(2)
+            if grad_norm > max_grad_norm then
+                print(grad_norm)
+                local shrink_factor = max_grad_norm / grad_norm
+                dparam:mul(shrink_factor)
+            end
+
+            return loss,dparam
         end
 
-        net:zeroGradParameters()
+        local _, err = optim.adam(feval,param,optim_config)
+        losses[i] = err[1]
 
-        resetStates()
+        image.save(string.format('vis/%05d.png', i), image.vflip(image.toDisplayTensor(net.output[1]:t())))
 
-        local f = criterion:forward(net:forward(inp),out)
-        net:backward(inp,criterion:backward(net.output,out))
-
-        local grad_norm = dparam:norm(2)
-        if grad_norm > max_grad_norm then
-            print(grad_norm)
-            local shrink_factor = max_grad_norm / grad_norm
-            dparam:mul(shrink_factor)
-        end
-
-        return f,dparam
+        print(i..': '..err[1])
     end
 
-    local _, err = optim.adam(feval,param,optim_config)
-    loss[i] = err[1]
+    val,idx = torch.max(net.output[1],2)
 
-    image.save(string.format('vis/%05d.png', i), image.vflip(image.toDisplayTensor(net.output[1]:t())))
+    gnuplot.pngfigure('plot_labels.png')
+    gnuplot.plot({'True',target_sequences,'-'}, {'Predicted',idx,'-'})
+    gnuplot.plotflush()
 
-    print(i..': '..err[1])
+    image.save('probs.png', image.vflip(image.toDisplayTensor(net.output[1]:t())))
+
+    gnuplot.pngfigure('loss_curve.png')
+    gnuplot.plot(losses,'-')
+    gnuplot.plotflush()
 end
 
-val,idx = torch.max(net.output[1],2)
-
-gnuplot.pngfigure('plot_labels.png')
-gnuplot.plot({'True',target_sequences,'-'}, {'Predicted',idx,'-'})
-gnuplot.plotflush()
-
-image.save('probs.png', image.vflip(image.toDisplayTensor(net.output[1]:t())))
-
-gnuplot.pngfigure('loss_curve.png')
-gnuplot.plot(loss,'-')
-gnuplot.plotflush()
+train()
