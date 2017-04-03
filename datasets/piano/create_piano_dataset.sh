5bc352211b4eeb58443cccfea5b8a4b48a9bc7db
Richard Assar <richard.assar@gmail.com>
2017-04-03 17:55:43 +0100

    Adding piano dataset generation script

---
+++ datasets/piano/create_piano_dataset.sh
@@ -0,0 +1,7 @@
+mkdir source/
+wget -r -H -nc -nH --cut-dir=1 -A .ogg -R *_vbr.mp3 -e robots=off -P source/ -l1 -i ./itemlist.txt -B 'http://archive.org/download/'
+mv source/*/*.ogg source/
+rm -r source/*/
+for file in source/*.ogg; do ffmpeg -i "$file" "source/`basename $file .ogg`.wav" && rm $file; done
+mkdir data/
+th ../../scripts/generate_dataset.lua -source_path source/ -dest_path data/
