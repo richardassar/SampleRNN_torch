cfa62450dcf4810316242e06e7ba3e0808f035fb
Richard Assar <richard.assar@gmail.com>
2017-04-03 16:57:31 +0100

    Adding scripts/generate_dataset.lua to assist with training set generation

---
+++ scripts/generate_dataset.lua
@@ -0,0 +1,57 @@
+require 'audio'
+require 'xlua'
+
+local cmd = torch.CmdLine()
+
+cmd:text('generate_dataset.lua options:')
+cmd:option('-source_path','','NCCL is enabled')
+cmd:option('-dest_path','','Resumes a previous training run if true')
+cmd:option('-seg_len',8,'The length in seconds of each audio segment')
+local args = cmd:parse(arg)
+
+assert(args.source_path:len() > 0, "source_path must be provided")
+assert(args.dest_path:len() > 0, "dest_path must be provided")
+assert(args.seg_len > 0, "seg_len must be positive")
+
+function get_files(path)    
+	local files = {}
+	for fname in paths.iterfiles(path) do
+		table.insert(files, path..'/'..fname)        
+	end
+
+	return files
+end
+
+print("Generating training set from '"..args.source_path.."'")
+local files = get_files("source")
+
+local idx = 1
+local sample_rate_check
+for i,filepath in pairs(files) do
+	print("Processing "..i.."/"..#files..":")
+
+	local aud,sample_rate = audio.load(filepath)
+	assert(sample_rate_check == nil or sample_rate_check == sample_rate, "Sample rate mismatch")
+	sample_rate_check = sample_rate
+
+	aud=aud:sum(2):view(-1) -- Mix stereo channels
+	aud:csub(aud:mean()) -- Remove DC component
+	aud:div(math.max(math.abs(aud:min()),aud:max())+1e-16) -- Normalize to abs-max amplitude
+	aud:add(1) -- Scale to [0,1]
+	aud:div(2)
+
+	local seglen_samples = args.seg_len * sample_rate
+	local n_segs = math.floor(aud:size(1)/seglen_samples)
+	for i=1,n_segs do
+		local aud = aud:narrow(1,(i-1)*seglen_samples+1,seglen_samples):view(-1,1)
+		if aud:min() ~= aud:max() then -- skip silence
+			aud = -0x80000000 + 0xFFFF0000 * aud
+			audio.save(string.format("%s/p%04d.wav",args.dest_path,idx), aud, sample_rate)
+			idx = idx + 1
+		end
+
+		xlua.progress(i,n_segs)
+	end
+end
+
+print("Done!")
