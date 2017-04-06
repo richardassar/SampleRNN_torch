5546adfac4252eec7a3bc81bd61137e54a93bd4f
Richard Assar <richard.assar@gmail.com>
2017-04-06 18:33:17 +0100

    Added create_violin_dataset.sh Minor changes to create_piano_dataset.sh

---
+++ datasets/violin/create_violin_dataset.sh
@@ -0,0 +1,13 @@
+mkdir source
+cd source
+wget "https://archive.org/compress/Op.124CapricesForSoloViolin/formats=OGG%20VORBIS&file=/Op.124CapricesForSoloViolin.zip"
+wget "https://archive.org/compress/213PartitaNo.2Chaconne/formats=OGG%20VORBIS&file=/213PartitaNo.2Chaconne.zip"
+wget "https://archive.org/compress/110SonateNo.3EnUtMajeurPour/formats=OGG%20VORBIS&file=/110SonateNo.3EnUtMajeurPour.zip"
+unzip \*.zip
+rm *.zip
+mv */*.ogg .
+for file in *.ogg; do ffmpeg -y -i "$file" -ac 1 -ar 22050 "${file%.ogg}.wav" && rm "$file"; done
+cd ..
+mkdir data/
+th ../../scripts/generate_dataset.lua -source_path source/ -dest_path data/
+rm -r source/
